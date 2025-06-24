4d83218df0c61c2cf85ea3106d9554a290b194c4
William Firmino <prof.williamfirmino@gmail.com>
2025-06-24 20:36:25 -0300

    Alterações depois do intervalo

--- main.js
+++ main.js
@@ -1,14 +1,7 @@
 const novaTarefa = document.querySelector('[data-form-button]');
-console.log( novaTarefa );
 
-const input = document.querySelector('[data-form-input]');
-const valor = input.value
-console.log( valor );
-
-// Eventos do DOM
-// Onde? , O que? , Quando?
-// Botão, fui clicado, quando o botão for clicado
-// click,  console.log("fui clicado") , novaTarefa
-// Toda vez que você utiliza um evento, esse é o padrão a ser seguido:
 novaTarefa.addEventListener('click', () => {
-    console.log("fui clicado")});
+    const input = document.querySelector('[data-form-input]');
+    const valor = input.value
+    console.log( valor );
+    });
