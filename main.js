98cc9cae6cb3762a1db335dc120f7cfad9d68604
William Firmino <prof.williamfirmino@gmail.com>
2025-06-26 19:45:43 -0300

    Alterações de antes do intervalo do dia 26/06/2025

--- main.js
+++ main.js
@@ -1,7 +1,25 @@
-const novaTarefa = document.querySelector('[data-form-button]');
+const criarTarefa = (evento) => {
 
-novaTarefa.addEventListener('click', () => {
+    evento.preventDefault();
+
+    const lista = document.querySelector('[data-list]');
     const input = document.querySelector('[data-form-input]');
     const valor = input.value
-    console.log( valor );
-    });
+    
+    const tarefa = document.createElement('li');
+    tarefa.classList.add('task');
+
+    const conteudo = `<p class="content">${valor}</p>`;
+
+    tarefa.innerHTML = conteudo;
+    lista.appendChild(tarefa);
+    input.value = " ";
+};
+
+const novaTarefa = document.querySelector('[data-form-button]');
+
+novaTarefa.addEventListener('click', criarTarefa);
+//Evento
+// Onde
+// Quando
+// O que
